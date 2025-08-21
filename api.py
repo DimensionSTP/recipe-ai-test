2157b28afef77710505640f245f354c2f2499d3f
DimensionSTP <ddang8jh@gmail.com>
2025-08-21 16:42:56 +0900

    add: api main executor

---
+++ api.py
@@ -0,0 +1,30 @@
+import dotenv
+
+dotenv.load_dotenv(
+    override=True,
+)
+
+import os
+import warnings
+
+os.environ["HYDRA_FULL_ERROR"] = "1"
+warnings.filterwarnings("ignore")
+
+import hydra
+from omegaconf import DictConfig
+
+from src.pipelines import api_pipeline
+
+
+@hydra.main(
+    config_path="configs/",
+    config_name="main.yaml",
+)
+def main(
+    config: DictConfig,
+) -> None:
+    return api_pipeline(config)
+
+
+if __name__ == "__main__":
+    main()
