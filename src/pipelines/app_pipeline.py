222ea54e5aeb9790893ee9f9770a9a563eb34d1e
DimensionSTP <ddang8jh@gmail.com>
2025-08-21 16:40:36 +0900

    add: app pipeline

---
+++ src/pipelines/app_pipeline.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+from omegaconf import DictConfig
+
+from ..utils import SetUp
+
+
+def pipeline(
+    config: DictConfig,
+) -> None:
+    setup = SetUp(config)
+
+    manager = setup.get_recommendation_manager()
+
+    st.title("Recipe AI Demo")
+
+    option = st.selectbox(
+        "Please select the recommendation mode",
+        [
+            "Lab number based recommendation",
+        ],
+    )
+
+    if option == "Lab number based recommendation":
+        lab_id = st.text_input("Enter the lab number to recommend for:")
+
+        if st.button("recommend"):
+            if not lab_id:
+                st.warning("Enter the lab number to recommend for.")
+            else:
+                results = manager.recommend_and_summarize(lab_id=lab_id)
+                st.subheader("Summary recommendation results")
+                st.write(results)
+
+    else:
+        raise ValueError("Invalid search mode")
