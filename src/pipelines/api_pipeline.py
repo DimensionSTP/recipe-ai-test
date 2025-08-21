7a2f48d6e81660b139544a2680984d7f9ce9bedf
DimensionSTP <ddang8jh@gmail.com>
2025-08-21 16:40:46 +0900

    add: api pipeline

---
+++ src/pipelines/api_pipeline.py
@@ -0,0 +1,29 @@
+from omegaconf import DictConfig
+
+from ..utils import SetUp
+
+
+def pipeline(
+    config: DictConfig,
+) -> None:
+    setup = SetUp(config)
+
+    manager = setup.get_recommendation_manager()
+
+    print("Recipe AI Demo")
+
+    option = "Lab number based recommendation"
+    print(f"Recommendation mode: {option}")
+
+    if option == "Lab number based recommendation":
+        lab_id = input("Enter the lab number to recommend for: ")
+
+        if not lab_id:
+            print("Warning: Enter the lab number to recommend for.")
+        else:
+            results = manager.recommend_and_summarize(lab_id=lab_id)
+            print("\nSummary recommendation results")
+            print(results)
+
+    else:
+        raise ValueError("Invalid search mode")
