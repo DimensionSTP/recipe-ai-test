61b86c1b51f437c16b1745af28d212443e7fbb08
DimensionSTP <ddang8jh@gmail.com>
2025-08-22 18:50:15 +0900

    refactor: set_vector_store script

--- scripts/set_vector_store.sh
+++ scripts/set_vector_store.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-path="src/preprocessing"
 indices_name="data_base_only.faiss"
 items_name="data_base_only.csv"
 
-python $path/set_vector_store.py \
+HYDRA_FULL_ERROR=1 python set_vector_store.py \
     indices_name=$indices_name \
     items_name=$items_name
